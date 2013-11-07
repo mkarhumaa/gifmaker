f05bfd4783072fcd192547a76be131e83e5ecaff
Matias Karhumaa <matias.karhumaa@gmail.com>
2013-11-07 09:41:36 +0200

    Code cleaning and documenting.

--- src/com/gifmakers/Converter.java
+++ src/com/gifmakers/Converter.java
@@ -24,7 +24,7 @@ import com.xuggle.xuggler.Global;
 public class Converter {
 
 	public static volatile boolean capturationDone = false;
-	public static final double RESIZE_FACTOR = 0.1;
+	public static double RESIZE_FACTOR = 1;
 	public static double FRAME_RATE;
 	private static String outputFilePrefix;
 
@@ -40,11 +40,13 @@ public class Converter {
 	// Time of last frame write
 	private static long mLastPtsWrite = Global.NO_PTS;
 	private static long mFirstFrame = -1;
-	public static long MICRO_SECONDS_BETWEEN_FRAMES; // = (long) (Global.DEFAULT_PTS_PER_SECOND * (1 / FRAME_RATE)); // SECONDS_BETWEEN_FRAMES);
+	public static long MICRO_SECONDS_BETWEEN_FRAMES;
 
 	public static void main(String[] args) {
 		try {
-			convert("/home/matias/Downloads/000.ts", "/home/matias/MMS/mysnapshot", 10, "00:00:00,000 --> 00:01:17,000");
+			convert("/home/matias/Downloads/000.ts",
+					"/home/matias/MMS/mysnapshot", 10,
+					"00:01:00,000 --> 00:01:17,000");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -53,55 +55,64 @@ public class Converter {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Converts given time interval of MPEG2 ts video segments to animated GIF.
-	 * @param inputFile MPEG2 ts video file to be converted
-	 * @param outputFile The prefix for output file. Method creates a animation and thumbnail using this prefix.
-	 * @param frameRate Frame rate of the output GIF animation.
-	 * @param timeInterval Time interval to be captured. Format is same than in SRT files: 00:00:00,000 --> 00:01:17,000
+	 * 
+	 * @param inputFile
+	 *            MPEG2 ts video file to be converted
+	 * @param outputFile
+	 *            The prefix for output file. Method creates a animation and
+	 *            thumbnail using this prefix.
+	 * @param frameRate
+	 *            Frame rate of the output GIF animation.
+	 * @param timeInterval
+	 *            Time interval to be captured. Format is same than in SRT
+	 *            files: 00:00:00,000 --> 00:01:17,000
 	 * @throws IOException
 	 * @throws InterruptedException
 	 * @throws RuntimeException
 	 */
-	public static void convert(String inputFile, String outputFile, double frameRate, String timeInterval ) throws IOException,
+	public static void convert(String inputFile, String outputFile,
+			double frameRate, String timeInterval) throws IOException,
 			InterruptedException, RuntimeException {
 		if (inputFile.equals("")) {
 			System.out.println("Illegal input filename.");
 			throw new RuntimeException();
 		}
-		
+
 		if (outputFile.equals("")) {
 			outputFilePrefix = "defaultoutput";
 		} else {
 			outputFilePrefix = outputFile;
 		}
-		
+
 		if (frameRate <= 0) {
 			System.out.println("Illegal frame rate.");
 			throw new RuntimeException();
 		} else {
 			FRAME_RATE = frameRate;
-			MICRO_SECONDS_BETWEEN_FRAMES = (long) (Global.DEFAULT_PTS_PER_SECOND * (1 / FRAME_RATE)); // SECONDS_BETWEEN_FRAMES);
+			MICRO_SECONDS_BETWEEN_FRAMES = (long) (Global.DEFAULT_PTS_PER_SECOND * (1 / FRAME_RATE));
 		}
-		
+
 		String[] interval = timeInterval.split(" --> ");
 		if (interval.length < 2) {
 			System.out.println("Error in time interval.");
 			return;
 		}
 		// Custom date format, eg. 00:00:10,500 --> 00:00:13,000"
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS");  
-        format.setTimeZone(TimeZone.getTimeZone("UTC"));
+		SimpleDateFormat format = new SimpleDateFormat(
+				"yyyy-MM-dd HH:mm:ss,SSS");
+		format.setTimeZone(TimeZone.getTimeZone("UTC"));
 
-        try {
+		try {
 			startTime = ((format.parse("1970-01-01 " + interval[0])).getTime() * 1000);
 			endTime = ((format.parse("1970-01-01 " + interval[1])).getTime() * 1000);
 		} catch (ParseException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+
 		biList = new ArrayList<BufferedImage>();
 
 		IMediaReader mediaReader = ToolFactory.makeReader(inputFile);
@@ -109,7 +120,7 @@ public class Converter {
 		// stipulate that we want BufferedImages created in BGR 24bit color
 		mediaReader
 				.setBufferedImageTypeToGenerate(BufferedImage.TYPE_3BYTE_BGR);
-		
+
 		mediaReader.addListener(new ImageSnapListener());
 
 		// Create output stream
@@ -124,39 +135,44 @@ public class Converter {
 
 		System.out.println("Starting to extract frames from video file.");
 		Date t0 = new Date();
-		
+
 		// read out the contents of the media file and
 		// dispatch events to the attached listener
 		try {
 			while (mediaReader.readPacket() == null && !capturationDone) {
-				if (biList.size() > (100/RESIZE_FACTOR)) {
+				if (biList.size() > (100 / RESIZE_FACTOR)) {
 					Thread.sleep(3000);
 				}
 			}
 		} catch (RuntimeException e) {
 			// Do nothing
 		}
-		
+
 		Date t1 = new Date();
-		System.out.println("Frames captured. Waiting for other thread to complete. It took " + (t1.getTime() - t0.getTime()) + "ms.");
+		System.out
+				.println("Frames captured. Waiting for other thread to complete. It took "
+						+ (t1.getTime() - t0.getTime()) + "ms.");
 		while (t.isAlive()) {
 			// Wait maximum of 3 second
-			// for MessageLoop thread
+			// for GifSequenceWriter thread
 			// to finish.
 			t.join(3000);
 		}
 		Date t2 = new Date();
-		System.out.println("Finally! It took " + (t2.getTime() - t0.getTime()) + "ms.");
-		
+		System.out.println("Finally! It took " + (t2.getTime() - t0.getTime())
+				+ "ms.");
 
 		output.close();
 	}
 
+	/**
+	 * Nested listener class for listening PictureEvents from IMediaReader.
+	 * 
+	 */
 	private static class ImageSnapListener extends MediaListenerAdapter {
 
 		public void onVideoPicture(IVideoPictureEvent event) {
 			if (event.getStreamIndex() != mVideoStreamIndex) {
-				System.out.println("Streamindex: " + event.getStreamIndex());
 				// if the selected video stream id is not yet set, go ahead an
 				// select this lucky video stream
 				if (mVideoStreamIndex == -1)
@@ -169,26 +185,23 @@ public class Converter {
 			if (mFirstFrame == -1) {
 				mFirstFrame = event.getTimeStamp();
 			}
-			
-			
+
 			// Check when we should start capturing
-			if (event.getTimeStamp() - mFirstFrame >= startTime && event.getTimeStamp() - mFirstFrame <= endTime) {
-				// if uninitialized, back date mLastPtsWrite to get the very first
-				// frame
+			if (event.getTimeStamp() - mFirstFrame >= startTime
+					&& event.getTimeStamp() - mFirstFrame <= endTime) {
+				// if uninitialized, back date mLastPtsWrite to get the very
+				// first frame
 				if (mLastPtsWrite == Global.NO_PTS)
 					mLastPtsWrite = event.getTimeStamp()
 							- MICRO_SECONDS_BETWEEN_FRAMES;
-	
+
 				// if it's time to write the next frame
 				if (event.getTimeStamp() - mLastPtsWrite >= MICRO_SECONDS_BETWEEN_FRAMES) {
-					
-					// String outputFilename = dumpImageToFile(event.getImage());
 					addImageToBuffer(event.getImage());
-	
 					// update last write time
 					mLastPtsWrite += MICRO_SECONDS_BETWEEN_FRAMES;
 				}
-			} 
+			}
 			// If we have already passed endTime, we can just stop seeking.
 			else if (event.getTimeStamp() - mFirstFrame > endTime) {
 				capturationDone = true;
@@ -196,31 +209,47 @@ public class Converter {
 
 		}
 
+		/**
+		 * This method adds BufferedImage image to the buffer to wait for
+		 * writing to GIF sequence. Also the resizing can be done by defining
+		 * RESIZE_FACTOR.
+		 * 
+		 * @param image
+		 *            BufferedImage to be written to buffer
+		 */
 		private void addImageToBuffer(BufferedImage image) {
 			int w, h;
 			BufferedImage resized;
-			//before = Converter.biList.remove(0);
 			w = image.getWidth();
 			h = image.getHeight();
-			resized = new BufferedImage((int)(RESIZE_FACTOR*w), (int)(RESIZE_FACTOR*h), BufferedImage.TYPE_INT_ARGB);
-			AffineTransform at = new AffineTransform();
-			at.scale(RESIZE_FACTOR, RESIZE_FACTOR);
-			AffineTransformOp scaleOp = 
-			   new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
-			resized = scaleOp.filter(image, resized);
-			
+			if (RESIZE_FACTOR != 1) {
+				resized = new BufferedImage((int) (RESIZE_FACTOR * w),
+						(int) (RESIZE_FACTOR * h), BufferedImage.TYPE_INT_ARGB);
+				AffineTransform at = new AffineTransform();
+				at.scale(RESIZE_FACTOR, RESIZE_FACTOR);
+				AffineTransformOp scaleOp = new AffineTransformOp(at,
+						AffineTransformOp.TYPE_BILINEAR);
+				resized = scaleOp.filter(image, resized);
+			} else {
+				resized = image;
+			}
 			biList.add(resized);
-			if (firstImage.equals("") == true) {				
+			if (firstImage.equals("") == true) {
 				firstImage = dumpImageToFile(resized);
 			}
 		}
 
+		/**
+		 * This method dumps BufferedImage to the png file.
+		 * 
+		 * @param image
+		 *            BufferedImage to be written to file.
+		 * @return String containing filename of the written imagefile.
+		 */
 		private String dumpImageToFile(BufferedImage image) {
 
 			try {
-				String outputFilename = outputFilePrefix
-						+ "_thumbnail.png";
-
+				String outputFilename = outputFilePrefix + "_thumbnail.png";
 				ImageIO.write(image, "png", new File(outputFilename));
 				return outputFilename;
 			} catch (IOException e) {
